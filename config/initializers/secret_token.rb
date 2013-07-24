ee93b4dc5345db77a249685e9faeaed624fdfb20
Diego Salazar <diego@greyrobot.com>
2013-07-24 04:24:05 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CartBootstrap::Application.config.secret_key_base = '430f2515cace22972e39ed3337c00f1564ab3fad409a9accb707328db2ddb70ba759f7113de0e1d33ed161a5e46ae764a6edd5b5e9b63de7038da878440098c0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CartBootstrap::Application.config.session_store :cookie_store, key: '_cart_bootstrap_session'
